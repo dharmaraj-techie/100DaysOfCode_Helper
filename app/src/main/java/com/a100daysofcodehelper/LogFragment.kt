64637a5dc9bdc71f69cac1e4f53de255f0549268
Dharmaraj <dharmaraj.techie@gmail.com>
2020-06-04 21:20:51 +0530

    add log fragment

---
+++ app/src/main/java/com/a100daysofcodehelper/LogFragment.kt
@@ -0,0 +1,23 @@
+package com.a100daysofcodehelper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * shows the logs as a list
+ */
+class LogFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
+
+}
