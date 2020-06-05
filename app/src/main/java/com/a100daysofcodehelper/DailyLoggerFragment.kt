359ed71ecd332bccfa306ce4d0e97b7a0a757a83
Dharmaraj <dharmaraj.techie@gmail.com>
2020-06-05 11:17:47 +0530

    add navigation graph and navigate to fragment

--- app/src/main/java/com/a100daysofcodehelper/DailyLoggerFragment.kt
+++ app/src/main/java/com/a100daysofcodehelper/DailyLoggerFragment.kt
@@ -1,10 +1,14 @@
 package com.a100daysofcodehelper
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_daily_loger.*
 
 
 /**
@@ -16,8 +20,31 @@ class DailyLoggerFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_daily_loger, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        submit_btn.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_dailyLoggerFragment_to_logFragment)
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        return inflater.inflate(R.menu.main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about_menu ->
+                findNavController().navigate(R.id.action_dailyLoggerFragment_to_aboutFragment)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
