72ef0beaac0d08586b7490fdf74c5dcfe19ccefa
Dharmaraj <dharmaraj.techie@gmail.com>
2020-07-16 18:34:23 +0530

    set onClickListener to fab

--- app/src/main/java/com/a100daysofcodehelper/screen/Stikes/ui/strike/StrikeFragment.kt
+++ app/src/main/java/com/a100daysofcodehelper/screen/Stikes/ui/strike/StrikeFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.a100daysofcodehelper.R
+import kotlinx.android.synthetic.main.strike_fragment.*
 
 class StrikeFragment : Fragment() {
 
@@ -20,13 +23,14 @@ class StrikeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewModel = ViewModelProvider(this).get(StrikeViewModel::class.java)
         return inflater.inflate(R.layout.strike_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        fabAddBtn.setOnClickListener(View.OnClickListener {
+            findNavController().navigate(StrikeFragmentDirections.actionStrikeFragmentToDailyLoggerFragment())
+        })
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(StrikeViewModel::class.java)
-        // TODO: Use the ViewModel
     }
-
 }
