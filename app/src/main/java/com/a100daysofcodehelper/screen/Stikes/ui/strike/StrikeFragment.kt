3942786475cf614aebe6e721a95067e95b0760cd
Dharmaraj <dharmaraj.techie@gmail.com>
2020-07-17 13:15:38 +0530

    add dataBinding to StrikeFragment.kt

--- app/src/main/java/com/a100daysofcodehelper/screen/Stikes/ui/strike/StrikeFragment.kt
+++ app/src/main/java/com/a100daysofcodehelper/screen/Stikes/ui/strike/StrikeFragment.kt
@@ -1,36 +1,48 @@
 package com.a100daysofcodehelper.screen.Stikes.ui.strike
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.a100daysofcodehelper.R
-import kotlinx.android.synthetic.main.strike_fragment.*
+import com.a100daysofcodehelper.databinding.DailyLogListItemBinding
+import com.a100daysofcodehelper.databinding.FragmentStrikeBinding
+import kotlinx.android.synthetic.main.fragment_strike.*
+import java.util.*
 
 class StrikeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StrikeFragment()
-    }
-
     private lateinit var viewModel: StrikeViewModel
+    private lateinit var binding: FragmentStrikeBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         viewModel = ViewModelProvider(this).get(StrikeViewModel::class.java)
-        return inflater.inflate(R.layout.strike_fragment, container, false)
-    }
+        binding = FragmentStrikeBinding.inflate(inflater,container,false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        fabAddBtn.setOnClickListener(View.OnClickListener {
+        binding.fabAddBtn.setOnClickListener(View.OnClickListener {
             findNavController().navigate(StrikeFragmentDirections.actionStrikeFragmentToDailyLoggerFragment())
         })
-        super.onActivityCreated(savedInstanceState)
+
+        calendarView.selectedDates = getSelectedDays()
+        return binding.root
+    }
+
+
+    private fun getSelectedDays(): List<Calendar> {
+        val calendars: MutableList<Calendar> =
+            ArrayList()
+        for (i in 0..9) {
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.DAY_OF_MONTH, i)
+            calendars.add(calendar)
+        }
+        return calendars
     }
 }
