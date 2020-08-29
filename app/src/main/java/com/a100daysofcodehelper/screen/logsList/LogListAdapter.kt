ba588c6012466a0dcf75c7b3d2ea394ef3a47f69
Dharmaraj <dharmaraj.techie@gmail.com>
2020-08-29 22:08:47 +0530

    add material design to the last log

--- app/src/main/java/com/a100daysofcodehelper/screen/logsList/LogListAdapter.kt
+++ app/src/main/java/com/a100daysofcodehelper/screen/logsList/LogListAdapter.kt
@@ -7,7 +7,7 @@ import com.a100daysofcodehelper.dataBase.DailyLog
 import com.a100daysofcodehelper.databinding.DailyLogListItemBinding
 import org.jetbrains.annotations.NotNull
 
-class LogListAdapter (): RecyclerView.Adapter<DailyLogViewHolder>() {
+class LogListAdapter : RecyclerView.Adapter<DailyLogViewHolder>() {
     var data = listOf<DailyLog>()
         set(value) {
             field = value
@@ -34,7 +34,6 @@ class DailyLogViewHolder(private val binding: DailyLogListItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(dailyLog: DailyLog){
-        binding.dailyLogTV.text = dailyLog.log
-        binding.dateTV.text = dailyLog.date
+        binding.dailyLog = dailyLog
     }
 }
